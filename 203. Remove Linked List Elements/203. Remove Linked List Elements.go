35d58f567f6bdd9e9f5e060e3696f13d61c041c4
Orenoid <stefu1225@gmail.com>
2022-01-02 21:07:21 +0800

    链表

---
+++ 203. Remove Linked List Elements/203. Remove Linked List Elements.go
@@ -0,0 +1,22 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	head = &ListNode{Next: head}
+	curr := head
+	for curr != nil && curr.Next != nil {
+		if curr.Next.Val == val {
+			curr.Next = curr.Next.Next
+		} else {
+			curr = curr.Next
+		}
+	}
+	return head.Next
+}
