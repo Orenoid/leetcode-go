b2aa7333b2188c32a3ec6661632700ac94416ec9
Orenoid <stefu1225@gmail.com>
2022-02-09 16:42:28 +0800

    二叉树

---
+++ leetcode/226.Invert-Binary-Tree/226. Invert Binary Tree.go
@@ -0,0 +1,26 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	var stack []*TreeNode
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node == nil {
+			continue
+		}
+		node.Left, node.Right = node.Right, node.Left
+		stack = append(stack, node.Right)
+		stack = append(stack, node.Left)
+	}
+	return root
+}
