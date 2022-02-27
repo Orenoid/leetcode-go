cb56466c7ecacac47c7500e87e34514b4bb4209f
Orenoid <stefu1225@gmail.com>
2022-02-27 22:37:09 +0800

    链表

--- leetcode/160.Intersection-of-Two-Linked-Lists/160. Intersection of Two Linked Lists.go
+++ leetcode/160.Intersection-of-Two-Linked-Lists/160. Intersection of Two Linked Lists.go
@@ -9,34 +9,18 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	if headA == nil || headB == nil {
 		return nil
 	}
-	lenA, lenB := 0, 0
 	currA, currB := headA, headB
-	for currA != nil {
-		lenA++
-		currA = currA.Next
+	for currA != currB {
+		if currA != nil {
+			currA = currA.Next
+		} else {
+			currA = headB
+		}
+		if currB != nil {
+			currB = currB.Next
+		} else {
+			currB = headA
+		}
 	}
-	for currB != nil {
-		lenB++
-		currB = currB.Next
-	}
-
-	var step int
-	var slow, fast *ListNode
-	if lenA > lenB {
-		step = lenA - lenB
-		slow, fast = headB, headA
-	} else {
-		step = lenB - lenA
-		slow, fast = headA, headB
-	}
-	for i := 0; i < step; i++ {
-		fast = fast.Next
-	}
-
-	for slow != fast {
-		slow = slow.Next
-		fast = fast.Next
-	}
-
-	return slow
+	return currA
 }
