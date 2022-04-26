7a28a09658f8a4cb18d564ce1a55a6d25bf75f0e
Orenoid <stefu1225@gmail.com>
2022-04-26 21:09:38 +0800

    update

--- leetcode/279.Perfect-Squares/279. Perfect Squares.go
+++ leetcode/279.Perfect-Squares/279. Perfect Squares.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func numSquares(n int) int {
 	dp := make([]int, n+1)
-	for i := 1; i <= n; i++ {
-		dp[i] = math.MaxInt
+	for j := 1; j <= n; j++ {
+		dp[j] = j
 	}
-	for i := 1; i <= n; i++ {
-		for j := 1; j*j <= n; j++ {
-			if i >= j*j {
-				dp[i] = min(dp[i], dp[i-j*j]+1)
-			}
+	for i := 2; i*i <= n; i++ {
+		for j := i * i; j <= n; j++ {
+			dp[j] = min(dp[j], dp[j-i*i]+1)
 		}
 	}
 	return dp[n]
