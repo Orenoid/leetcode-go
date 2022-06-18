98054838e8ca5a7a7aab3efca01f926a60dd206b
Orenoid <stefu1225@gmail.com>
2022-06-18 12:32:39 +0800

    双指针

--- leetcode/76.Minimum-Window-Substring/76. Minimum Window Substring.go
+++ leetcode/76.Minimum-Window-Substring/76. Minimum Window Substring.go
@@ -1,58 +1,45 @@
 package main
 
+import "math"
+
 func minWindow(s string, t string) string {
 	if len(s) == 0 || len(t) == 0 {
 		return ""
 	}
-	cache := make(map[rune]int, len(t))
-	for _, ch := range t {
-		cache[ch]++
+	currCounter, tCounter := map[byte]int{}, map[byte]int{}
+	for i := range t {
+		tCounter[t[i]]++
 	}
-	containsAllChar := func() bool {
-		for _, count := range cache {
-			if count > 0 {
+	check := func() bool {
+		for ch, count := range tCounter {
+			if currCounter[ch] < count {
 				return false
 			}
 		}
 		return true
 	}
 
+	ansL, ansR := -1, -1
 	left, right := 0, 0
-	minWinLeft, minWinRight := -1, -1
-	minLength := len(s) + 1
-
-	for right < len(s) {
-		if _, ok := cache[rune(s[right])]; ok {
-			cache[rune(s[right])]--
-		}
-		if containsAllChar() {
+	minLength := math.MaxInt64
+	for ; right < len(s); right++ {
+		currCounter[s[right]]++
+		for check() && left <= right {
 			if right-left+1 < minLength {
-				minLength = left + right + 1
-				minWinLeft, minWinRight = left, right
+				minLength = right - left + 1
+				ansL, ansR = left, right
 			}
-			for left < right {
-				if _, found := cache[rune(s[left])]; found {
-					cache[rune(s[left])]++
-				}
-				left++
-				if containsAllChar() {
-					if right-left+1 < minLength {
-						minLength = right - left + 1
-						minWinLeft, minWinRight = left, right
-					}
-				} else {
-					break
-				}
-			}
+			currCounter[s[left]]--
+			left++
 		}
-		right++
 	}
-	if minWinLeft == -1 {
+	if ansL == -1 {
 		return ""
 	}
-	return s[minWinLeft : minWinRight+1]
+	return s[ansL : ansR+1]
+
 }
 
 func main() {
-	println(minWindow("a", "aa"))
+	println(minWindow("ab", "b"))
 }
